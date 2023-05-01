d4e9dce1342ca5baf679409f7fd3d6e749653602
정제우 <wpdn3825@naver.com>
2023-05-01 13:47:51 +0900

    zustand 연습

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
-  return <div>helloworld!</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/say')
+      .then((response) => setData(response.data.say[0].id))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return <div>{data}</div>;
 }
 
 export default App;
