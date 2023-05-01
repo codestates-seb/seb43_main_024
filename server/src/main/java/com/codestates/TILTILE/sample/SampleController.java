34994ffef56b32acb90f26e1e4b2aa69e5961e25
[KwonPyo Hong] <[pyo9501@gmail.com]>
2023-05-01 14:20:14 +0900

    aws test

--- server/src/main/java/com/codestates/TILTILE/sample/SampleController.java
+++ server/src/main/java/com/codestates/TILTILE/sample/SampleController.java
@@ -1,13 +1,14 @@
 package com.codestates.TILTILE.sample;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/sample")
 public class SampleController {
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    @GetMapping("/")
+    public ResponseEntity postSample() {
+        return new ResponseEntity<>(new ResponseDto("Hello World"), HttpStatus.CREATED);
     }
 }
