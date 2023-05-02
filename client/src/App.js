4b603f821610b10e9e6d9833968bb2f9bda3c696
정제우 <wpdn3825@naver.com>
2023-05-02 11:34:42 +0900

    test build 작성

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,24 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState('');
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/say')
-      .then((response) => setData(response.data.say[0].id))
+      .get('/sample/')
+      .then((response) => {
+        const parsedData = JSON.parse(response.data);
+        setData(parsedData.message);
+      })
       .catch((error) => console.error(error));
   }, []);
 
-  return <div>{data}</div>;
+  return (
+    <div>
+      <h1>{data}</h1>
+      <p>위에 helloworld 가 입력되지 않는다면 연결에 실패한 것입니다.</p>
+    </div>
+  );
 }
 
 export default App;
