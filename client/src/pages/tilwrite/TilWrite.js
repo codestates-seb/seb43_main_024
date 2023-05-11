f5ebf1aebf6cbf1a6cf4ab93b87766b23f621c64
HDEUN <pikadoss1@gmail.com>
2023-05-11 16:57:16 +0900

    md-editor 장착/추 후 사진첨부 작업 필요

--- client/src/pages/tilwrite/TilWrite.js
+++ client/src/pages/tilwrite/TilWrite.js
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import '@uiw/react-md-editor/markdown-editor.css';
+import { InnerWrapper } from '../../default/styled';
+
 function TilWrite() {
-  return <>틸작성</>;
+  const [value, setValue] = useState();
+
+  return (
+    <InnerWrapper>
+      <MDEditor value={value} onChange={setValue} />
+      <div style={{ marginTop: '16px' }}>
+        <MDEditor.Markdown source={value} />
+      </div>
+    </InnerWrapper>
+  );
 }
 
 export default TilWrite;
