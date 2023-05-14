ad257e820298453246e66dda8abe7afaa262756c
CDSeoHyunJun <sjmsjm0302@naver.com>
2023-05-14 21:41:41 +0900

    fix : 버그 수정

--- server/src/main/java/com/codestates/TILTILE/email/EmailService.java
+++ server/src/main/java/com/codestates/TILTILE/email/EmailService.java
@@ -1,9 +1,7 @@
 package com.codestates.TILTILE.email;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
--- server/src/main/java/com/codestates/TILTILE/member/mapper/MemberMapper.java
+++ server/src/main/java/com/codestates/TILTILE/member/mapper/MemberMapper.java
@@ -3,13 +3,20 @@ package com.codestates.TILTILE.member.mapper;
 import com.codestates.TILTILE.member.dto.MemberDto;
 import com.codestates.TILTILE.member.entity.Member;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MemberMapper {
-    @Mapping(source = "email", target = "email")
-    @Mapping(source = "nickName", target = "nickName")
-    @Mapping(source = "password", target = "password")
-    Member memberPostToMember(MemberDto.Post requestBody);
+    default Member memberPostToMember(MemberDto.Post memberDto) {
+        if (memberDto == null) {
+            return null;
+        }
+
+        Member member = new Member();
+        member.setEmail(memberDto.getEmail());
+        member.setNickName(memberDto.getNickName());
+        member.setPassword(memberDto.getPassword());
+
+        return member;
+    }
 }
