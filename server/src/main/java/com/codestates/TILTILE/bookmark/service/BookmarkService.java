27495f187505c841fc57e17db73073b96d3e1c42
CDSeoHyunJun <sjmsjm0302@naver.com>
2023-05-22 15:55:18 +0900

    feat : 페이지네이션 추가

--- server/src/main/java/com/codestates/TILTILE/bookmark/service/BookmarkService.java
+++ server/src/main/java/com/codestates/TILTILE/bookmark/service/BookmarkService.java
@@ -44,30 +44,46 @@ public class BookmarkService {
         return bookmarkRepository.findByMember(member);
     }
 
-    public MemberWithBookmarksDto getMemberWithBookmarks(Long memberId) {
+    public MemberWithBookmarksDto getMemberWithBookmarks(Long memberId, int page, int pageSize) {
+        // 회원 ID로 회원 정보를 가져옴
         Member member = memberService.getMemberById(memberId);
         if (member == null) {
+            // 회원이 존재하지 않을 경우 NotFoundException 예외를 던짐
             throw new NotFoundException(HttpStatus.NOT_FOUND.value(), "Member not found for ID: " + memberId);
         }
+
+        // 회원의 북마크 목록을 가져옴
         List<Bookmark> bookmarks = getBookmarksByMember(member);
+        // 북마크를 생성된 날짜를 기준으로 내림차순으로 정렬
         bookmarks.sort(Comparator.comparing(Bookmark::getCreatedAt).reversed());
 
+        // 전체 북마크 수
+        int totalBookmarks = bookmarks.size();
+        // 페이지의 시작 인덱스
+        int startIndex = (page - 1) * pageSize;
+        // 페이지의 끝 인덱스 (실제 북마크 목록의 크기를 초과하지 않도록 함)
+        int endIndex = Math.min(startIndex + pageSize, totalBookmarks);
+        // 페이지에 해당하는 북마크 목록 추출
+        List<Bookmark> pagedBookmarks = bookmarks.subList(startIndex, endIndex);
+
+        // 페이지에 해당하는 북마크의 TIL 정보를 가져와서 리스트에 추가
         List<Til> tilList = new ArrayList<>();
-        for (Bookmark bookmark : bookmarks) {
+        for (Bookmark bookmark : pagedBookmarks) {
             Long tilId = bookmark.getTil().getTilId();
             Til til = tilService.getTilById(tilId);
             tilList.add(til);
         }
 
+        // 결과를 담을 MemberWithBookmarksDto 객체 생성 및 필드 설정
         MemberWithBookmarksDto dto = new MemberWithBookmarksDto();
-        dto.setMemberId(member.getMemberId());
-        dto.setNickName(member.getNickName());
-//        dto.setTilTier(member.getTilTier());
-//        dto.setAboutMe(member.getAboutMe());
-        dto.setBookmarks(bookmarks);
+        dto.setMemberId(member.getMemberId()); // 회원 ID 설정
+        dto.setNickName(member.getNickName()); // 회원 닉네임 설정
+        // 필요에 따라 다른 속성들을 설정 (주석에서는 생략되었음)
+        dto.setBookmarks(pagedBookmarks); // 페이지네이션된 북마크 목록 설정
 
-        return dto;
+        return dto; // 완성된 DTO 반환
     }
+
 
     public void deleteBookmark(Long bookmarkId) {
         bookmarkRepository.deleteById(bookmarkId);
