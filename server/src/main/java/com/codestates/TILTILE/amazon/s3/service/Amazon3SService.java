47774f8ca5c137919d97d25b83c77b27a967fa3e
CDSeoHyunJun <sjmsjm0302@naver.com>
2023-05-23 12:46:23 +0900

    feat : 프로필 이미지 기능 추가

--- server/src/main/java/com/codestates/TILTILE/amazon/s3/service/Amazon3SService.java
+++ server/src/main/java/com/codestates/TILTILE/amazon/s3/service/Amazon3SService.java
@@ -1,11 +1,14 @@
 package com.codestates.TILTILE.amazon.s3.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -13,9 +16,11 @@ import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.codestates.TILTILE.amazon.s3.dto.S3FileDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,14 +38,11 @@ public class Amazon3SService {
     /**
      * S3로 파일 업로드
      */
-    public List<S3FileDto> uploadFiles(String fileType, List<MultipartFile> multipartFiles) {
-
-        List<S3FileDto> s3files = new ArrayList<>();
-
+    public List<S3FileDto> uploadFiles(String fileType, List<MultipartFile> multipartFiles) throws FileUploadException {
+        List<S3FileDto> s3Files = new ArrayList<>();
         String uploadFilePath = fileType + "/" + getFolderName();
 
         for (MultipartFile multipartFile : multipartFiles) {
-
             String originalFileName = multipartFile.getOriginalFilename();
             String uploadFileName = getUuidFileName(originalFileName);
             String uploadFileUrl = "";
@@ -50,59 +52,32 @@ public class Amazon3SService {
             objectMetadata.setContentType(multipartFile.getContentType());
 
             try (InputStream inputStream = multipartFile.getInputStream()) {
-
-                String keyName = uploadFilePath + "/" + uploadFileName; // ex) 구분/년/월/일/파일.확장자
+                String keyName = uploadFilePath + "/" + uploadFileName;
+                PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, keyName, inputStream, objectMetadata);
 
                 // S3에 폴더 및 파일 업로드
-                amazonS3Client.putObject(
-                        new PutObjectRequest(bucketName, keyName, inputStream, objectMetadata));
-
-                // TODO : 외부에 공개하는 파일인 경우 Public Read 권한을 추가, ACL 확인
-        /*amazonS3Client.putObject(
-            new PutObjectRequest(bucket, s3Key, inputStream, objectMetadata)
-                .withCannedAcl(CannedAccessControlList.PublicRead));*/
+                PutObjectResult putObjectResult = amazonS3Client.putObject(putObjectRequest);
 
                 // S3에 업로드한 폴더 및 파일 URL
                 uploadFileUrl = amazonS3Client.getUrl(bucketName, keyName).toString();
 
+                s3Files.add(S3FileDto.builder()
+                        .originalFileName(originalFileName)
+                        .uploadFileName(uploadFileName)
+                        .uploadFilePath(uploadFilePath)
+                        .uploadFileUrl(uploadFileUrl)
+                        .build());
             } catch (IOException e) {
                 e.printStackTrace();
-                log.error("Filed upload failed", e);
+                log.error("File upload failed", e);
+                // 파일 업로드 실패에 대한 예외 처리
+                throw new FileUploadException("File upload failed");
             }
-
-            s3files.add(
-                    S3FileDto.builder()
-                            .originalFileName(originalFileName)
-                            .uploadFileName(uploadFileName)
-                            .uploadFilePath(uploadFilePath)
-                            .uploadFileUrl(uploadFileUrl)
-                            .build());
         }
 
-        return s3files;
+        return s3Files;
     }
 
-    /**
-     * S3에 업로드된 파일 삭제
-     */
-    public String deleteFile(String uploadFilePath, String uuidFileName) {
-
-        String result = "success";
-
-        try {
-            String keyName = uploadFilePath + "/" + uuidFileName; // ex) 구분/년/월/일/파일.확장자
-            boolean isObjectExist = amazonS3Client.doesObjectExist(bucketName, keyName);
-            if (isObjectExist) {
-                amazonS3Client.deleteObject(bucketName, keyName);
-            } else {
-                result = "file not found";
-            }
-        } catch (Exception e) {
-            log.debug("Delete File failed", e);
-        }
-
-        return result;
-    }
 
     /**
      * UUID 파일명 반환
@@ -120,5 +95,23 @@ public class Amazon3SService {
         Date date = new Date();
         String str = sdf.format(date);
         return str.replace("-", "/");
+    }
+
+    public void deleteFileByUrl(String fileUrl) {
+        // S3에서 파일 삭제
+        String key = extractKeyFromUrl(fileUrl);
+        amazonS3Client.deleteObject(bucketName, key);
+    }
+
+    private String extractKeyFromUrl(String fileUrl) {
+        // 파일 URL에서 키 추출
+        // 예: https://s3.amazonaws.com/bucket-name/file.jpg 에서 "bucket-name/file.jpg" 추출
+        URL url;
+        try {
+            url = new URL(fileUrl);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("Invalid file URL");
+        }
+        return url.getPath().substring(1);
     }
 }
