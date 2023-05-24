540ca21bf56884def86ea44af8ac883a4643daca
CDSeoHyunJun <sjmsjm0302@naver.com>
2023-05-24 11:55:05 +0900

    refactor : claim에 memberId, nickName 추가

--- server/src/main/java/com/codestates/TILTILE/auth/config/SecurityConfiguration.java
+++ server/src/main/java/com/codestates/TILTILE/auth/config/SecurityConfiguration.java
@@ -87,7 +87,7 @@ public class SecurityConfiguration {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(List.of("http://tiltil2-images.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000"));
-        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "memberId"));
         corsConfiguration.setExposedHeaders(List.of("Authorization", "Refresh"));
         corsConfiguration.setAllowedMethods(List.of("POST", "GET", "PATCH", "PUT", "DELETE", "OPTIONS"));
 
