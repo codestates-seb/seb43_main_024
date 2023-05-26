64311fbb35882f95b290f8ca49dbf256150ef552
정제우 <wpdn3825@naver.com>
2023-05-26 16:58:22 +0900

    배포버젼 1

--- client/src/pages/profile/components/UserProfile.js
+++ client/src/pages/profile/components/UserProfile.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import API from '../../../API';
 import jwt_decode from 'jwt-decode';
 import { UserProfileWrapper, ImgBox } from '../../../default/styled';
 import IconPencil from '../../../default/image/ico-pencil.svg';
-import Loading from '../../../default/image/loading.gif';
 
 export function UserProfile() {
   const [profileData, setProfileData] = useState(null);
@@ -26,14 +25,18 @@ export function UserProfile() {
     fetchProfileData();
   }, []);
 
-  return (
-    <React.Suspense fallback={<div>{Loading}</div>}>
-      <UserProfileContent profileData={profileData} />
-    </React.Suspense>
-  );
-}
+  // null 에 대한 loading 처리
+  if (!profileData) {
+    return (
+      <>
+        <div>Loading...</div>;
+        <Link to="/editpass">
+          <button>정보수정</button>
+        </Link>
+      </>
+    );
+  }
 
-function UserProfileContent({ profileData }) {
   return (
     <UserProfileWrapper>
       <div className="flexCenter">
@@ -45,10 +48,11 @@ function UserProfileContent({ profileData }) {
           />
         </ImgBox>
         <h2>{profileData.nickName}</h2>
+        {/* <h3>나의 다짐</h3> */}
         <p>
-          {profileData?.aboutMe
+          {profileData.aboutMe
             ? profileData.aboutMe
-            : `저는 개발자가 되기위한 ${profileData?.nickName} 입니다 최고의 개발자가 되는날 까지 열심히 Til을 기록하겠습니다.`}
+            : `저는 개발자가 되기위한 ${profileData.nickName} 입니다 최고의 개발자가 되는날 까지 열심히 Til을 기록하겠습니다.`}
         </p>
       </div>
       <Link to="/editpass">
