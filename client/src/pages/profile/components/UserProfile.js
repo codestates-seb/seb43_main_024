2a7280ae5ac43ae8fd415c8ccffce7c9ab3e69df
정제우 <wpdn3825@naver.com>
2023-05-26 16:35:37 +0900

    profile loading 컴포넌트 삽입

--- client/src/pages/profile/components/UserProfile.js
+++ client/src/pages/profile/components/UserProfile.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import API from '../../../API';
 import jwt_decode from 'jwt-decode';
 import { UserProfileWrapper } from '../../../default/styled';
 import IconPencil from '../../../default/image/ico-pencil.svg';
+import Loading from '../../../default/image/loading.gif';
 
 export function UserProfile() {
   const [profileData, setProfileData] = useState(null);
@@ -25,32 +26,28 @@ export function UserProfile() {
     fetchProfileData();
   }, []);
 
-  // null 에 대한 loading 처리
-  if (!profileData) {
-    return (
-      <>
-        <div>Loading...</div>;
-        <Link to="/editpass">
-          <button>정보수정</button>
-        </Link>
-      </>
-    );
-  }
+  return (
+    <React.Suspense fallback={<div>{Loading}</div>}>
+      <UserProfileContent profileData={profileData} />
+    </React.Suspense>
+  );
+}
 
+function UserProfileContent({ profileData }) {
   return (
     <UserProfileWrapper>
       <div className="flexCenter">
         <img
           className="user-photo"
-          src={profileData.img ? profileData.img : '/defaultprofile.png'}
+          src={profileData?.img ? profileData.img : '/defaultprofile.png'}
           alt="user profile"
         />
-        <h2>{profileData.nickName}</h2>
+        <h2>{profileData?.nickName}</h2>
         {/* <h3>나의 다짐</h3> */}
         <p>
-          {profileData.aboutMe
+          {profileData?.aboutMe
             ? profileData.aboutMe
-            : `저는 개발자가 되기위한 ${profileData.nickName} 입니다 최고의 개발자가 되는날 까지 열심히 Til을 기록하겠습니다.`}
+            : `저는 개발자가 되기위한 ${profileData?.nickName} 입니다 최고의 개발자가 되는날 까지 열심히 Til을 기록하겠습니다.`}
         </p>
       </div>
       <Link to="/editpass">
