6efb07db9ed1d829319229360032eb3c1d26046f
CDSeoHyunJun <sjmsjm0302@naver.com>
2023-06-27 00:59:22 +0900

    refactor : 틸틸이 티어 기능 구현

--- server/src/main/java/com/codestates/TILTILE/batch/jobs/TilTierJobConfiguration.java
+++ server/src/main/java/com/codestates/TILTILE/batch/jobs/TilTierJobConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
